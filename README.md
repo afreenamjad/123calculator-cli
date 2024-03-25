344f30629970e213a698fce03c3035b84a3b80dc
afreenamjad <160919150+afreenamjad@users.noreply.github.com>
2024-03-25 09:41:17 -0700

    push my project
    it is my first project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# code-with-afreen-cli-caculator" 
---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+console.log("welcome to\ code afreen\ cli-calcultor")
+
+import inquirer from "inquirer"; 
+import Choice from "inquirer/lib/objects/choice.js";
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one off the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "subtraction", "multipliction", "Division"],
+  },
+]);
+//conditional statment
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multipliction") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("please select valid operator");
+}
